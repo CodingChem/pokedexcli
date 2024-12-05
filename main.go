cc264a2956c92d386cf92e0c5e8668fe8d8ae705
Vegard Seines <vegsei@gmail.com>
2024-12-05 16:29:32 +0100

    Add: main.go with basic repl

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Printf("pokedex->")
+		scanner.Scan()
+		err := scanner.Err()
+		if err != nil {
+			fmt.Println("encountered fatal error, shutting down...")
+			return
+		}
+		command := scanner.Text()
+		CommandMap[command].callback()
+	}
+}
